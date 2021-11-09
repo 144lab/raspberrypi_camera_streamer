51d460949cc6ca7e0c4bb6cd417c4b326befcf61
sasaki.yusuke <sasaki.yusuke@144lab.com>
2021-11-09 09:06:07 +0900

    コマンド引数を追加

--- stream.py
+++ stream.py
@@ -10,6 +10,7 @@ from threading import Condition
 from http import server
 
 stream_status = False
+stream_path = ''
 
 class StreamingOutput(object):
     def __init__(self):
@@ -30,7 +31,7 @@ class StreamingOutput(object):
 
 class StreamingHandler(server.BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == '/stream.mjpg':
+        if self.path == stream_path:
             self.send_response(200)
             self.send_header('Age', 0)
             self.send_header('Cache-Control', 'no-cache, private')
@@ -60,14 +61,23 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     allow_reuse_address = True
     daemon_threads = True
 
-def run(port = 8001, rotation = 0):
-    global output, server, stream_status
-    with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
+def run(listen, port, path, width, height, framerate, quality, rotation, vflip, hflip, **kwargs):
+    global output, server, stream_status, stream_path
+    stream_path = path
+    # with picamera.PiCamera(resolution='1280x720', framerate=60) as camera:
+    # with picamera.PiCamera(resolution='1920x1080', framerate=30) as camera:
+    # with picamera.PiCamera(resolution='640x480', framerate=90) as camera:
+    with picamera.PiCamera() as camera:
         output = StreamingOutput()
+        camera.resolution = (width,height)
+        camera.framerate = framerate
         camera.rotation = rotation
-        camera.start_recording(output, format='mjpeg')
+        camera.hflip = hflip
+        camera.vflip = vflip
+        camera.start_recording(output, format='mjpeg', quality=max(min(quality,100),1))
+        print(camera.frame, camera.resolution,camera.framerate,camera.vflip,camera.hflip,camera.rotation)
         try:
-            address = ('', port)
+            address = (listen, port)
             stream_status = True
             server = StreamingServer(address, StreamingHandler)
             server.serve_forever()
@@ -85,4 +95,17 @@ def stop():
     server.server_close()
 
 if __name__ == "__main__":
-    run()
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--listen", type=str, default="")
+    parser.add_argument("--port", type=int, default=8080)
+    parser.add_argument("--path", type=str, default="/stream.mjpg")
+    parser.add_argument("--width", type=int, default=1920)
+    parser.add_argument("--height", type=int, default=1080)
+    parser.add_argument("--framerate", type=int, default=30)
+    parser.add_argument("--quality", type=int, default=100)
+    parser.add_argument("--hflip", action='store_true')
+    parser.add_argument("--vflip", action='store_true')
+    parser.add_argument("--rotation", type=int, default=0)
+    args = parser.parse_args()
+    run(**vars(args))
